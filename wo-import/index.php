75de917fe7ab307380d2cff40367fd4c504b6586
SLC <uc@slokvenko.com>
2021-03-05 15:13:13 +0200

    Woo import 0 list of post to import

--- wo-import/index.php
+++ wo-import/index.php
@@ -4,6 +4,29 @@
 <?php
 $productsItems = new \SlcShop\Controller\ProductGroups();
 foreach ($productsItems->productGroupsData as $productGroupData) {
-    $productGroup = new \SlcShop\Model\ProductGroup($productGroupData);
-    echo $productGroup->getTitle() . '<br>';
+    $productGroup = new \SlcShop\Model\ProductGroup(null, $productGroupData);
+    echo $productGroup->getTitle() . '<br>'; ?>
+    <br>
+    <br>
+    <div class="">Products params:</div>
+    <?php
+    foreach ($productGroup->paramsData as $paramData) {
+        $productParam = new \SlcShop\Model\ProductParam(null, $paramData);
+        echo $productParam->getTitle() . '<br>';
+    }
 }
+?>
+<br>
+<br>
+<div class="">Products list (<?php echo $productGroup->getTitle() ?>):</div>
+<br>
+<?php
+$productPosts = get_posts([
+    'numberposts' => -1,
+    'post_type' => $productGroup->getSlug(),
+]);
+
+foreach ($productPosts as $productPost) {
+    echo $productPost->post_title . '<br>';
+}
+
